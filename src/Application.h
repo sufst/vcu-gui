72beb31623ae3d35ec9bd0d0b7224ab39f97bf54
Tim Brewis <timbrewis27@gmail.com>
2022-06-20 17:43:18 +0100

    refactor(gui): move main window class to its own file

--- src/Application.h
+++ src/Application.h
@@ -9,7 +9,7 @@
 #include <JuceHeader.h>
 #include <limits.h>
 
-#include "MainComponent.h"
+#include "MainWindow.h"
 
 /**
  * @brief Main JUCE application
@@ -28,48 +28,6 @@ public:
     const juce::String getApplicationName() override;
     const juce::String getApplicationVersion() override;
     bool moreThanOneInstanceAllowed() override;
-
-    //==============================================================================
-    /*
-        This class implements the desktop window that contains an instance of
-        our MainComponent class.
-    */
-    class MainWindow : public juce::DocumentWindow
-    {
-      public:
-        MainWindow(juce::String name)
-            : DocumentWindow(
-                name,
-                juce::Desktop::getInstance().getDefaultLookAndFeel().findColour(
-                    juce::ResizableWindow::backgroundColourId),
-                DocumentWindow::allButtons)
-        {
-            setUsingNativeTitleBar(true);
-            setContentOwned(new MainComponent(), true);
-            setResizeLimits(minWidth, minHeight, INT_MAX, INT_MAX);
-
-#if JUCE_IOS || JUCE_ANDROID
-            setFullScreen(true);
-#else
-            setResizable(true, true);
-            centreWithSize(getWidth(), getHeight());
-#endif
-
-            setVisible(true);
-        }
-
-        void closeButtonPressed() override
-        {
-            JUCEApplication::getInstance()->systemRequestedQuit();
-        }
-
-      private:
-        // constants
-        static const int minWidth = 500;
-        static const int minHeight = 350;
-
-        JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainWindow)
-    };
 
 private:
 
---
+++ src/MainWindow.h
@@ -0,0 +1,30 @@
+/******************************************************************************
+ * @file   MainWindow.h
+ * @author Tim Brewis (@t-bre, tab1g19@soton.ac.uk)
+ * @brief  Main GUI window
+ *****************************************************************************/
+
+#pragma once
+
+#include <JuceHeader.h>
+
+/**
+ * @brief Main GUI window
+ */
+class MainWindow : public juce::DocumentWindow
+{
+public:
+
+    MainWindow(const juce::String& name);
+
+    void closeButtonPressed() override;
+
+private:
+
+    static const int minWidth = 500;
+    static const int minHeight = 350;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainWindow)
+
+
+};
