dcb7d2c43ed2a3e106ad5d558fb033cfd9b4e140
Tim Brewis <timbrewis27@gmail.com>
2022-07-23 21:16:40 +0100

    feat(gui): create outline 'Look And Feel' class

---
+++ src/gui/appearance/LookAndFeel.cpp
@@ -0,0 +1,21 @@
+/******************************************************************************
+ * @file   LookAndFeel.cpp
+ * @author Tim Brewis (@t-bre, tab1g19@soton.ac.uk)
+ * @brief  Look and feel
+ *****************************************************************************/
+
+#include "LookAndFeel.h"
+
+namespace sufst
+{
+
+/**
+ * @brief Constructor
+ */
+LookAndFeel::LookAndFeel()
+{
+    
+}
+
+
+} // namespace sufst
---
+++ src/gui/appearance/LookAndFeel.h
@@ -0,0 +1,29 @@
+/******************************************************************************
+ * @file   LookAndFeel.h
+ * @author Tim Brewis (@t-bre, tab1g19@soton.ac.uk)
+ * @brief  Look and feel
+ *****************************************************************************/
+
+#pragma once
+
+#include <JuceHeader.h>
+
+namespace sufst
+{
+
+/**
+ * @brief Look and feel derived from the JUCE default
+ */
+class LookAndFeel : public juce::LookAndFeel_V4
+{
+public:
+
+    LookAndFeel();
+
+private:
+
+    JUCE_DECLARE_NON_COPYABLE(LookAndFeel)
+
+};
+
+} // namespace sufst
